757604a146fcc74a45e8ea119a089ce327b10a38
sherloxk <sherloxk5254@gmail.com>
2025-11-02 13:37:58 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:oumua/core/services/log_service.dart';
 import 'package:oumua/locator.dart';
 import 'package:oumua/router/app_router.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,6 +9,7 @@ import 'package:global_configuration/global_configuration.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GlobalConfiguration().loadFromAsset("app_settings");
+  LogService.init();
   setupLocator();
   GetIt.instance.registerSingleton<AppRouter>(AppRouter());
   runApp(MyApp());
